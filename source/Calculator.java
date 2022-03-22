73d79c922348a429056f13f65a7fe9688d90b745
준규 <vpark45@gmail.com>
2022-03-23 00:06:40 +0900

    Add Calculator class

---
+++ source/Calculator.java
@@ -0,0 +1,21 @@
+class Calculator
+{
+  public static String expression = "";
+  public static int currentCharPosition = 0;
+  public static char Look;
+
+  public static void GetChar()
+  {
+    if (currentCharPosition < expression.length())
+      Look = expression.charAt(currentCharPosition);
+
+    currentCharPosition++;
+  }
+
+  public static void main(String args[])
+  {
+    expression = "1";
+    GetChar();
+    System.out.println(Look);
+  }
+}
