9100cebec9a4969d7c322316ccbd2c2b5d95e51d
준규 <vpark45@gmail.com>
2022-03-26 00:42:29 +0900

    Add Token class

---
+++ source/Token.java
@@ -0,0 +1,16 @@
+class Token
+{
+  public String text;
+  public String type;
+
+  public Token(String text, String type)
+  {
+    this.text = text;
+    this.type = type;
+  }
+
+  public String toString()
+  {
+    return "Text: " + text + " Type: " + type;
+  }
+}
