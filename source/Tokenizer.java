03d670882224fd05ac01b3739fd44b61886745fe
준규 <vpark45@gmail.com>
2022-03-26 01:08:09 +0900

    Add PrettyPrint method and modify main method

--- source/Tokenizer.java
+++ source/Tokenizer.java
@@ -99,13 +99,38 @@ class Tokenizer
     return tokens;
   }
 
+  public void PrettyPrint(List<Token> tokens)
+  {
+    int numberCount = 0;
+    int opCount = 0;
+
+    for (Token token : tokens)
+    {
+      if (token.type.equals("NUMBER"))
+      {
+        System.out.println("Number....: " + token.text);
+        numberCount++;
+      }
+      else
+      {
+        System.out.println("Operator..: " + token.type);
+        opCount++;
+      }
+    }
+    System.out.println("You have got " + numberCount +
+                       " different number and " + opCount
+                       + " operators.");
+  }
+
   public static void main(String[] args)
   {
     String expression = "219+341+19";
     expression += " ";
+
     Tokenizer tokenizer = new Tokenizer();
     List<Token> tokens = tokenizer.Tokenize(expression);
-    for (Token token : tokens)
-      System.out.println("Type: " + token.type + " Text: " + token.text);
+
+    System.out.println("--------------");
+    tokenizer.PrettyPrint(tokens);
   }
 }
